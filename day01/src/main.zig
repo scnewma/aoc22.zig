75ce146dd41b9fe93db6bff5025d3e488baba8f0
Shaun Newman <newman350z@gmail.com>
2023-06-24 12:20:18 -0700

    refactors "heap" to a TopN data type

--- day01/src/main.zig
+++ day01/src/main.zig
@@ -6,14 +6,14 @@ pub fn main() !void {
     var reader = stdin.reader();
 
     var elf_calories: u32 = 0;
-    var top3 = [3]u32{ 0, 0, 0 };
+    var top3 = TopN(u32, 3).new();
 
     while (true) {
         var buffer: [8]u8 = undefined;
         const line = try reader.readUntilDelimiterOrEof(&buffer, '\n');
         if (line) |ln| {
             if (ln.len == 0) {
-                insTop3(&top3, elf_calories);
+                top3.insert(elf_calories);
                 elf_calories = 0;
                 continue;
             }
@@ -21,26 +21,49 @@ pub fn main() !void {
             elf_calories += calories;
         } else {
             // ensure we count the last elf
-            insTop3(&top3, elf_calories);
+            top3.insert(elf_calories);
             break;
         }
     }
 
-    try stdout.writer().print("Part 01: {}\n", .{top3[2]});
-
-    var top3Sum: u32 = 0;
-    for (top3) |n| {
-        top3Sum += n;
-    }
-    try stdout.writer().print("Part 02: {}\n", .{top3Sum});
+    try stdout.writer().print("Part 01: {}\n", .{top3.max()});
+    try stdout.writer().print("Part 02: {}\n", .{top3.sum()});
 }
 
-fn insTop3(top3: *[3]u32, num: u32) void {
-    var n = num;
-    for (0..3) |idx| {
-        const i = 2 - idx;
-        const min = @min(top3[i], n);
-        top3[i] = @max(top3[i], n);
-        n = min;
-    }
+// n could theoretically be anything, but TopN doesn't actually implement the
+// heap algorithm so you will want to keep the number of elements low
+fn TopN(comptime T: type, comptime N: u8) type {
+    return struct {
+        elems: [N]T,
+
+        const Self = @This();
+
+        pub fn new() Self {
+            return Self{
+                .elems = std.mem.zeroes([N]T),
+            };
+        }
+
+        pub fn insert(self: *Self, elem: T) void {
+            var e = elem;
+            for (0..N) |idx| {
+                const i = 2 - idx;
+                const min = @min(self.elems[i], e);
+                self.elems[i] = @max(self.elems[i], e);
+                e = min;
+            }
+        }
+
+        pub fn max(self: *Self) T {
+            return self.elems[N - 1];
+        }
+
+        pub fn sum(self: *Self) T {
+            var s: u32 = 0;
+            for (self.elems) |e| {
+                s += e;
+            }
+            return s;
+        }
+    };
 }
